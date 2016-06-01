49994f499b089a990167eab66dabd0abcbfc5631
colod <colod@DESKTOP-54KJ3D4>
2016-06-01 10:35:15 +0800

    添加项目文件。

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+   class Program
+    {
+        static void Main(string[] args)
+     {
+            Student stu = new Student(1,"hello");
+           
+            Console.WriteLine(stu.ID + " "+stu.Name);
+     }
+
+   
+    }
+    class Student
+    {
+        public Student(int initld,string initName)
+        {
+            this.ID = initld;
+            this.Name = initName;
+        }
+        public Student()
+        {
+            this.
+        }
+        public int ID;
+        public string Name;
+    }
+
+}
